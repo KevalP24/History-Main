697c1e9eda17c9616dfb627d74524168ee136a00
KevalP24 <kevalrp2004@gmail.com>
2024-05-09 23:04:17 +0530

    commited changes

--- script/script.js
+++ script/script.js
@@ -2,25 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
   const accordionBtns = document.querySelectorAll('.accordion-btn');
 
   accordionBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-      const panel = this.nextElementSibling;
-      const panels = document.querySelectorAll('.panel');
-      const buttons = document.querySelectorAll('.accordion-btn'); 
-      
-      panels.forEach(p => {
-        if (p !== panel && p.classList.contains('active')) {
-          p.classList.remove('active');
-        }
-      });
+      btn.addEventListener('click', function() {
+          const panel = this.nextElementSibling;
+          const panels = document.querySelectorAll('.panel');
+          const buttons = document.querySelectorAll('.accordion-btn');
 
-      buttons.forEach(b => { 
-        if (b !== this && b.classList.contains('active')) {
-          b.classList.remove('active');
-        }
-      });
+          panels.forEach(p => {
+              if (p !== panel && p.classList.contains('active')) {
+                  p.classList.remove('active');
+              }
+          });
 
-      panel.classList.toggle('active');
-      this.classList.toggle('active');
-    });
+          buttons.forEach(b => {
+              if (b !== this && b.classList.contains('active')) {
+                  b.classList.remove('active');
+              }
+          });
+
+          panel.classList.toggle('active');
+          this.classList.toggle('active');
+      });
   });
 });
