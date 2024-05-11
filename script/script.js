51c8ed06eaf3a0eb1523310c37cc5f359f75ec47
KevalP24 <kevalrp2004@gmail.com>
2024-05-11 14:19:54 +0530

    cimmiteds

--- script/script.js
+++ script/script.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const accordionBtns = document.querySelectorAll('.accordion-btn');
+    const accordionBtns = document.querySelectorAll('.accordion-btn');
 
-  accordionBtns.forEach(btn => {
-      btn.addEventListener('click', function() {
-          const panel = this.nextElementSibling;
-          const panels = document.querySelectorAll('.panel');
-          const buttons = document.querySelectorAll('.accordion-btn');
+    accordionBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            const panel = this.nextElementSibling;
+            const panels = document.querySelectorAll('.panel');
+            const buttons = document.querySelectorAll('.accordion-btn');
 
-          panels.forEach(p => {
-              if (p !== panel && p.classList.contains('active')) {
+            panels.forEach(p => {
+                if (p !== panel && p.classList.contains('active')) {
                   p.classList.remove('active');
-              }
-          });
-
-          buttons.forEach(b => {
-              if (b !== this && b.classList.contains('active')) {
+                }
+            });
+            buttons.forEach(b => {
+                if (b !== this && b.classList.contains('active')) {
                   b.classList.remove('active');
-              }
-          });
+                }
+            });
 
-          panel.classList.toggle('active');
-          this.classList.toggle('active');
-      });
-  });
+            panel.classList.toggle('active');
+            this.classList.toggle('active');
+        });
+    });
 });
 
